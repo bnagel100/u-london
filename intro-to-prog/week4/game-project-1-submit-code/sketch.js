f51d796af520a34032cca9d0d6277e79fcf244c5
bnagel100 <bnagel100@hotmail.com>
2020-12-06 15:00:19 -0600

    Project6start

--- intro-to-prog/week4/game-project-1-submit-code/sketch.js
+++ intro-to-prog/week4/game-project-1-submit-code/sketch.js
@@ -39,33 +39,29 @@ function draw()
 	rect(0, 432, 1024, 144); //draw some green ground
 
 	//1. a cloud in the sky
-	//... add your code here
 
-	noStroke();
-	fill(255);
-//	text("cloud", 200, 100);
-
+    fill(255);
     ellipse(200,150,80,80)
     ellipse(160,150,60,60)
     ellipse(240,150,60,60)
     ellipse(130,150,40,40)
     ellipse(270,150,40,40)
 
+    noStroke();
+	fill(255);
+    text("cloud", 200, 100);
+
 	//2. a mountain in the distance
-	//... add your code here
 
     fill(128,128,129);
-
-
     triangle(450,432,750,432,600,200)
     triangle(350,432,550,432,450,300)
 
     noStroke();
 	fill(255);
-//	text("mountain", 500, 256);
+    text("mountain", 500, 256);
 
 	//3. a tree
-	//... add your code here
 
     fill(120,100,40)
     rect(900,282,60,150)
@@ -77,17 +73,11 @@ function draw()
 
 	noStroke();
 	fill(255);
-//	text("tree", 800, 346);
+    text("tree", 800, 346);
 
 
 	//4. a canyon
 	//NB. the canyon should go from ground-level to the bottom of the screen
-
-	//... add your code here
-
-	noStroke();
-	fill(255);
-	// text("canyon", 100, 480);
 
     //canyon dark green
     stroke(0,120,0);
@@ -115,30 +105,12 @@ function draw()
     noStroke();
     fill(100,155,255)
     rect(70,402,80,30)
-  /* fill(120,130,40)
-    rect(100,432,60,150)
 
-    fill(120,90,40)
-    rect(100,432,10,150)
-
-    fill(120,90,40)
-    rect(150,432,10,150)
-
-
-    fill(120,105,40)
-    rect(110,432,10,150)
-
-    fill(120,105,40)
-    rect(140,432,10,150)
-
-    fill(120,105,40)
-    rect(140,432,10,150) */
-	//5. a collectable token - eg. a jewel, fruit, coins
-	//... add your code here
-
-	noStroke();
+    	noStroke();
 	fill(255);
-//	text("collectable item", 400, 400);
+	text("canyon", 100, 480);
+
+	//5. a collectable token - eg. a jewel, fruit, coins
 
     fill(255,215,0)
     ellipse(400,400,20,20)
@@ -146,5 +118,9 @@ function draw()
     rect(425,385,5,20)
     rect(435,385,5,20)
     rect(445,385,5,20)
+
+    noStroke();
+	fill(255);
+	text("collectable item", 420, 370);
 }
 
